85e31578c5ca2885f3ad17a950098f1b0e805c7b
Kevin-269581661 <269581661@qq.com>
2019-07-20 14:16:37 +0800

    demo

---
+++ vue-preview-photos/src/components/demo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      list: [],
+      previewPictureArray: [
+        {
+          fullThumbnailPath: '',
+          fullPath: ''
+        },
+      ]
+    }
+  }
+}
+</script>
+<style lang="stylus">
+  
+</style>
